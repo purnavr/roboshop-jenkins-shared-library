6dec2d8aec94b3f506dc9d9a1ef77445bcb7fc3f
purna <purnaraomba@gmail.com>
2024-05-09 15:31:51 +0530

    Roboshop Update

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -35,7 +35,7 @@ def call() {
 
     post {
       failure {
-        mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'friendscreations634@gmail.com', subject: "${component} - Pipeline Failed", to: 'friendscreations634@gmail.com'
+        mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'friendscreations634@gmail.com', mimeType: 'text/html', subject: "${component} - Pipeline Failed", to: 'friendscreations634@gmail.com'
       }
     }
   }
