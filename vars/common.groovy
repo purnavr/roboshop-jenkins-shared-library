5e0f6f2b44ba03205f8f5b465f4048d837ba490a
purna <purnaraomba@gmail.com>
2024-05-15 14:25:28 +0530

    Roboshop Update

--- vars/common.groovy
+++ vars/common.groovy
@@ -15,10 +15,14 @@ def compile() {
 //  sh 'echo OK'
 //}
 
+//def codequality() {
+//  withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
+//    sh 'sonar-scanner -Dsonar.host.url=http://172.31.80.191:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.sourceEncoding=utf-8'
+//  }
+//}
+
 def codequality() {
-  withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
-    sh 'sonar-scanner -Dsonar.host.url=http://172.31.80.191:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.sourceEncoding=utf-8'
-  }
+  sh 'sonar-scanner -Dsonar.host.url=http://172.31.80.191:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
 }
 
 
