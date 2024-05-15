16106d9559f7b0d9a5d8685eb3b69f269340dd58
purna <purnaraomba@gmail.com>
2024-05-15 14:33:55 +0530

    Roboshop Update

--- vars/common.groovy
+++ vars/common.groovy
@@ -7,6 +7,15 @@ def compile() {
   }
 }
 
+def testcases() {
+//  npm test
+//  maven test
+//  python -m unittests
+//  go test
+//  sh 'echo OK'
+}
+
+
 def codequality() {
   sh 'sonar-scanner -Dsonar.host.url=http://172.31.80.191:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts} -Dsonar.qualitygate.wait=true'
 }
