b51224589bdd9e5ac66606b62d6e516870a75a41
purna <purnaraomba@gmail.com>
2024-05-28 16:33:37 +0530

    Roboshop Update

--- vars/common.groovy
+++ vars/common.groovy
@@ -25,11 +25,10 @@ def codequality() {
 
 def prepareArtifacts() {
   sh 'echo ${TAG_NAME} >VERSION'
-  if (app_lang == "nodejs" || app_lang == "angular" ) {
-  sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile.groovy'
-  }
   if (app_lang == "maven") {
     sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
+  } else {
+    sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile.groovy'
   }
 }
 
@@ -60,6 +59,10 @@ def ArtifactUpload() {
 //  if (app_lang == "angular") {
 //    sh 'zip -r ${component}-${TAG_NAME}.zip * VERSION -x Jenkinsfile.groovy'
 //  }
+//}
+
+//if (app_lang == "nodejs" || app_lang == "angular" ) {
+//  sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile.groovy'
 //}
 
 
