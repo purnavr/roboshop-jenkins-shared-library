55ad8260b2b10e07e228866cb685e593fca07e3d
purna <purnaraomba@gmail.com>
2024-06-26 20:41:02 +0530

    Roboshop Update

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -19,7 +19,7 @@ def call() {
           script {
             env.SSH_PASSWORD = sh ( script: 'aws ssm get-parameter --name prod.ssh.pass --with-decryption --query Parameter.Value | xargs', returnStdout: true ).trim()
             wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: SSH_PASSWORD]]])  {
-              sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1 >/tmp/servers'
+              sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers'
               sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=root -e ansible_password=${SSH_PASSWORD}'
             }
           }
