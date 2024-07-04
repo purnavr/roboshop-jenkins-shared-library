77cf7ede83a93ed24df4d4be3d22ca23782835dd
purna <purnaraomba@gmail.com>
2024-07-04 23:54:22 +0530

    Roboshop Update

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,3 +1,4 @@
+/*
 def call() {
   pipeline {
     agent any
@@ -30,6 +31,59 @@ def call() {
             }
           }
 
+        }
+      }
+    }
+
+    post {
+      always {
+        cleanWs()
+      }
+    }
+
+  }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def call() {
+  pipeline {
+    agent any
+
+    options {
+      ansiColor('xterm')
+    }
+
+    parameters {
+      string(name: 'app_version', defaultValue: '', description: 'App Version')
+      string(name: 'component', defaultValue: '', description: 'Component')
+      string(name: 'environment', defaultValue: '', description: 'Environment')
+    }
+
+    stages {
+      stage('Update Parameter Store') {
+        steps {
+          sh 'aws ssm put-parameter --name ${environment}.${component}.app_version --type "String" --value "${app_version}" --overwrite'
+        }
+      }
+      stage('Deploy Servers') {
+        steps {
+          sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers'
+          sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment}'
         }
       }
     }
