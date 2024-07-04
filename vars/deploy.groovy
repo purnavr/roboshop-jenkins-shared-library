21655610398af4adc5ae5735e73dfe5229b4dd3d
purna <purnaraomba@gmail.com>
2024-07-05 00:42:31 +0530

    Roboshop Update

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,64 +1,3 @@
-//def call() {
-//  pipeline {
-//    agent any
-//
-//    options {
-//      ansiColor('xterm')
-//    }
-//
-//    parameters {
-//      string(name: 'app_version', defaultValue: '', description: 'App Version')
-//      string(name: 'component', defaultValue: '', description: 'Component')
-//      string(name: 'environment', defaultValue: '', description: 'Environment')
-//    }
-//
-//    stages {
-//      stage('Update Parameter Store') {
-//        steps {
-//          sh 'aws ssm put-parameter --name ${environment}.${component}.app_version --type "String" --value "${app_version}" --overwrite'
-//        }
-//      }
-//      stage('Deploy Servers') {
-//        steps {
-//          script {
-//            env.SSH_PASSWORD = sh ( script: 'aws ssm get-parameter --name prod.ssh.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
-//            wrap([$class: 'MaskPasswordsBuildWrapper',
-//                  varPasswordPairs: [[password: SSH_PASSWORD]]]) {
-//              sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers'
-//              sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=root -e ansible_password=${SSH_PASSWORD}'
-//
-//            }
-//          }
-//
-//        }
-//      }
-//    }
-//
-//    post {
-//      always {
-//        cleanWs()
-//      }
-//    }
-//
-//  }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def call() {
   pipeline {
     agent any
@@ -81,8 +20,16 @@ def call() {
       }
       stage('Deploy Servers') {
         steps {
-          sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers'
-          sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=root -e ansible_password=DevOps321'
+          script {
+            env.SSH_PASSWORD = sh ( script: 'aws ssm get-parameter --name prod.ssh.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+            wrap([$class: 'MaskPasswordsBuildWrapper',
+                  varPasswordPairs: [[password: SSH_PASSWORD]]]) {
+              sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers'
+              sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=root -e ansible_password=${SSH_PASSWORD}'
+
+            }
+          }
+
         }
       }
     }
@@ -95,3 +42,56 @@ def call() {
 
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//def call() {
+//  pipeline {
+//    agent any
+//
+//    options {
+//      ansiColor('xterm')
+//    }
+//
+//    parameters {
+//      string(name: 'app_version', defaultValue: '', description: 'App Version')
+//      string(name: 'component', defaultValue: '', description: 'Component')
+//      string(name: 'environment', defaultValue: '', description: 'Environment')
+//    }
+//
+//    stages {
+//      stage('Update Parameter Store') {
+//        steps {
+//          sh 'aws ssm put-parameter --name ${environment}.${component}.app_version --type "String" --value "${app_version}" --overwrite'
+//        }
+//      }
+//      stage('Deploy Servers') {
+//        steps {
+//          sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers'
+//          sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=root -e ansible_password=DevOps321'
+//        }
+//      }
+//    }
+//
+//    post {
+//      always {
+//        cleanWs()
+//      }
+//    }
+//
+//  }
+//}
