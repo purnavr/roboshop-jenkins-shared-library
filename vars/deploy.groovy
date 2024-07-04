3c8f5a0ef47149381eb6acd8516ef06c846b725b
purna <purnaraomba@gmail.com>
2024-07-05 00:47:57 +0530

    Roboshop Update

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -59,39 +59,4 @@ def call() {
 
 
 
-//def call() {
-//  pipeline {
-//    agent any
-//
-//    options {
-//      ansiColor('xterm')
-//    }
-//
-//    parameters {
-//      string(name: 'app_version', defaultValue: '', description: 'App Version')
-//      string(name: 'component', defaultValue: '', description: 'Component')
-//      string(name: 'environment', defaultValue: '', description: 'Environment')
-//    }
-//
-//    stages {
-//      stage('Update Parameter Store') {
-//        steps {
-//          sh 'aws ssm put-parameter --name ${environment}.${component}.app_version --type "String" --value "${app_version}" --overwrite'
-//        }
-//      }
-//      stage('Deploy Servers') {
-//        steps {
-//          sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text |xargs -n1>/tmp/servers'
-//          sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=root -e ansible_password=DevOps321'
-//        }
-//      }
-//    }
-//
-//    post {
-//      always {
-//        cleanWs()
-//      }
-//    }
-//
-//  }
-//}
+
