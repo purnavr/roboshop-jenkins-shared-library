321d1b05cec5dfed24262a8b2373e244bca6549f
purna <purnaraomba@gmail.com>
2024-08-16 16:27:33 +0530

    Roboshop Update

---
+++ vars/helm.groovy
@@ -0,0 +1,59 @@
+def call() {
+  pipeline {
+    agent any
+
+    options {
+      ansiColor('xterm')
+    }
+
+    parameters {
+      string(name: 'app_version', defaultValue: '', description: 'App Version')
+      string(name: 'component', defaultValue: '', description: 'Component')
+      string(name: 'environment', defaultValue: '', description: 'Environment')
+    }
+
+    stages {
+      stage('Clone Application') {
+        steps {
+          dir ('APP') {
+            git branch: 'main', url: "https://github.com/purnavr/${component}.git"
+          }
+        }
+      }
+
+      stage('Deploy Helm Chart') {
+        steps {
+          script {
+            sh 'helm upgrade -i ${component} . -f APP/helm/${environment}.yaml --set appversion=${app_version}'
+          }
+        }
+      }
+
+    }
+
+    post {
+      always {
+        cleanWs()
+      }
+    }
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
